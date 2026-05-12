2c32552a47ed01b4c40e095b78119033f17da708
Romanbot4 <tice.lover1500@gmail.com>
2026-05-12 12:03:09 +0630

    feat: - added a newly designed landing page

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://romanbot4.github.io',
+    base: '/bliss_box_public/',
+});
