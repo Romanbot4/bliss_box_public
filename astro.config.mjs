3823fe0d0fc72f4da6118e36f2ae87cc9151c0e3
Romanbot4 <tice.lover1500@gmail.com>
2026-05-13 15:23:31 +0630

    feat: - updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+const isNetlify = process.env.NETLIFY === 'true';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://romanbot4.github.io',
-    base: '/bliss_box_public/',
+    site: isNetlify ? 'https://blissbox.netlify.app' : 'https://romanbot4.github.io',
+    base: isNetlify ? '/' : '/bliss_box_public/',
 });
